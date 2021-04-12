bae38d3ec708e7b58840afe72bbd187809cd649f
Liying Yin <yinliying53@gmail.com>
2021-04-12 12:37:50 -0700

    add signup page

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template
+from flask import request, redirect
+from flaskext.mysql import MySQL
 import os
 
 #Configuration
 app = Flask(__name__)
 
 #Routes
+#Landing page
 @app.route('/')
 def root():
     return render_template('landing_page.j2')
+
+#sign up page
+@app.route('/signup', methods=['GET','POST'])
+def signup_post():
+    if request.method == 'GET':
+        return render_template('signup_form_page.j2')
+    elif request.method == 'POST':
+        return render_template('landing_page.j2')
 
 #Listener
 if __name__ == "__main__":
