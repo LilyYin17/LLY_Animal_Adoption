5613752b85c5e12b96d84b17cfce3a01d98c0883
Liying Yin <yinliying53@gmail.com>
2021-04-20 15:09:30 -0700

    modify admin home page, implement Dogs archive

--- pets/dogs.py
+++ pets/dogs.py
@@ -9,3 +9,8 @@ dogs_api = Blueprint('dogs_api', __name__)
 @dogs_api.route('/browse_dogs')
 def browse_dogs():
     return render_template('browse_dogs.j2')
+
+#Dogs Archive page, show all dogs
+@dogs_api.route('/dogs_archive')
+def dogs_archive():
+    return render_template('dogs_archive.j2')
