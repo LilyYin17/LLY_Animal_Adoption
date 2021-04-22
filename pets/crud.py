7d85d98cdebe3abeb2f1211775532b99a1c8e2a3
Liying Yin <yinliying53@gmail.com>
2021-04-22 12:27:25 -0700

    add crud.py

--- pets/crud.py
+++ pets/crud.py
@@ -10,4 +10,5 @@ def add_new_pets():
     if request.method == 'GET':
         return render_template('add_new_pets.j2')
     elif request.method == 'POST':
+        db_connection = db.db_connection
         return render_template('browse_dogs.j2')
