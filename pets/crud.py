2d99d5c27761c4c6faaddac00a327dad322799a5
Tingting Lin <lintin@oregonstate.edu>
2021-04-25 10:04:59 -0700

    implement the find your pet page for admin

--- pets/crud.py
+++ pets/crud.py
@@ -41,31 +41,10 @@ def admin_find_your_pet():
     elif request.method == 'POST':
         db_connection = db.db_connection
 
-        query = 'SELECT * FROM Pets WHERE type = "%s";' % (221)
+        # query = 'SELECT * FROM Pets WHERE type = "%s" and breed = "%s" and name = "%s";' % (221)
+        # query = 'SELECT * FROM Pets WHERE type = "%s";'
+        query = 'SELECT * FROM Pets WHERE type = "dog";'
         cursor = db.execute_query(db_connection, query)
         results = cursor.fetchall()
-        return render_template('dogs_archive.j2', dogs=results)
+        return render_template('admin_detailed_find_your_pet.j2', pets=results, base64=base64)
 
-
-
-
-
-        query = 'INSERT INTO Pets(type, name, img, breed, age, size, gender, goodWithKids, goodWithDogs, goodWithCats, mustBeLeashed, availability) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-        type = request.form['type']
-        name = request.form['name']
-        img = request.files['img'].read()
-        breed = request.form['breed']
-        age = request.form['age']
-        size = request.form['size']
-        gender = request.form['gender']
-        goodWithKids = request.form['goodWithKids']
-        goodWithDogs = request.form['goodWithDogs']
-        goodWithCats = request.form['goodWithCats']
-        mustBeLeashed = request.form['mustBeLeashed']
-        availability = request.form['availability']
-
-        data = (type, name, img, breed, age, size, gender, goodWithKids, goodWithDogs, goodWithCats, mustBeLeashed, availability)
-        db.execute_query(db.db_connection, query, data)
-        
-        # return redirect(url_for('admin_new_pets'))
-        return redirect(url_for('admin_add_new_pet_result'))
